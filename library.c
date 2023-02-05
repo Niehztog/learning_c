6ce62cdcd225368349ea0e4c8730abc06834d4dc
Nils Gotzhein <nils.gotzhein@googlemail.com>
2023-02-05 13:07:29 +0100

    load a dynamic library and execute function

---
+++ library.c
@@ -0,0 +1,6 @@
+//
+// Created by nilsg on 05.02.2023.
+//
+int some_function(int param1, int param2) {
+    return param1 + param2;
+}
---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <windows.h>
+
+int load_lib();
+typedef int (*FunctionPointer)(int, int);
+
+int main() {
+    printf("Hello, World!\n");
+    int result = load_lib();
+    return result;
+}
+
+int load_lib() {
+    HINSTANCE hinstLib;
+    FunctionPointer func;
+    BOOL fFreeResult;
+
+    // Load the dynamic library
+    hinstLib = LoadLibrary("library.dll");
+    if (hinstLib == NULL) {
+        printf("Could not load the library\n");
+        return 1;
+    }
+
+    // Get the function from the library
+    func = (FunctionPointer)GetProcAddress(hinstLib, "some_function");
+    if (func == NULL) {
+        printf("Could not find the function\n");
+        return 1;
+    }
+
+    // Call the function from the library
+    int result = func(3, 5);
+    printf("Result from function: %d\n", result);
+
+    // Free the dynamic library
+    fFreeResult = FreeLibrary(hinstLib);
+    if (!fFreeResult) {
+        printf("Could not free the library\n");
+        return 1;
+    }
+
+    return 0;
+}
